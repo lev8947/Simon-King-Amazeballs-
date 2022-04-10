16513672f0b90d2e270ef0816379697e7cbcd267
Simon Levey <lev_47@hotmail.com>
2022-04-10 22:05:19 +0800

    added files

---
+++ main.js
@@ -0,0 +1,51 @@
+let scene, camera, renderer, cube;
+
+function init(){
+scene = new THREE.Scene();
+
+camera = new THREE.PerspectiveCamera(
+    10,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+renderer = new THREE.WebGLRenderer({antialias: true});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(2, 2, 2);
+//const material = new THREE.MeshBasicMaterial({ color: 0x00ffff });
+
+const texture = new THREE.TextureLoader().load('textures/cube.jpg');
+const material = new THREE.MeshBasicMaterial({ map: texture });
+cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+}
+
+
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.001;
+    cube.rotation.y += 0.001;
+
+    renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+init();
+animate();
